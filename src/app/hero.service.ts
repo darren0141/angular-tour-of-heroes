05f58cf6e0bb16d41936f3967443e53442c19b7c
darren0141 <darrencowan95@gmail.com>
2022-06-13 11:19:05 +0100

    make heroDetails into its own component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,12 +9,18 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor(private MessageService: MessageService) { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
-    this.MessageService.add('HeroService: fetched heroes')
+    this.messageService.add('HeroService: fetched heroes')
     return heroes;
   }
-  
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+
 }
